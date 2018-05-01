e85e0b68f412767bd68cf0f320d381dc2eac117e
Ben Turner <bbgrabbag@gmail.com>
2018-04-30 19:19:53 -0600

    finished non-quiz coursework

--- routes/api/course-material/index.js
+++ routes/api/course-material/index.js
@@ -39,8 +39,30 @@ courseMaterialRouter.route("/:id")
     .get((req, res) => {
         CourseMaterialModel.findById(req.params.id, (err, materials) => {
             if (err) return res.status(500).send(err);
+            if (!materials) return res.status(404).send({ message: "Material not found" })
             res.status(200).send(materials);
         })
+    })
+    .put((req, res) => {
+        if (req.user.permissions.admin) {
+            CourseMaterialModel.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedMat) => {
+                if (err) return res.status(500).send(err);
+                if (!updatedMat) return res.status(404).send({ message: "Material not found" });
+                res.status(200).send(updatedMat);
+            })
+        } else {
+            res.status(401).send({ message: "Admin authorization required" })
+        }
+    })
+    .delete((req, res) => {
+        if (req.user.permissions.admin) {
+            CourseMaterialModel.findByIdAndRemove(req.params.id, (err) => {
+                if (err) return res.status(500).send(err);
+                res.status(204).send();
+            })
+        } else {
+            res.status(401).send({ message: "Admin authorization required" })
+        }
     })
 
 
--- routes/api/course-material/questions.js
+++ routes/api/course-material/questions.js
@@ -2,5 +2,20 @@ const express = require("express");
 const questionRouter = express.Router({ mergeParams: true });
 
 questionRouter.route("/")
+// BASIC
+// get all questions belonging to assignment / query
+
+// ADMIN
+// post question (text, mult)
+// delete all q's to assignment
+// edit all q's to assignment
+
+// BASIC
+// get one q
+
+// ADMIN
+//  delete one q
+//  edit one q
+
     
 module.exports = questionRouter;
