e98e82923d855d7b0a8e6406a1b6f40fa6f46f6a
Ben Turner <bbgrabbag@gmail.com>
2018-05-05 17:08:20 -0600

    days are now posted using insertMany

--- routes/api/days.js
+++ routes/api/days.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const dayRouter = express.Router();
 
+//models
 const { Day, InSessionDay } = require("../../models/api/days");
+
+//utils
+const { daySortCallback } = require("./utils");
 
 dayRouter.route("/")
     .get((req, res) => {
-        Day.find(req.query, (err, days) => {
-            if (err) return res.send(err);
-            res.status(200).send(days);
-        });
+        if (req.user.admin) {
+            Day.find(req.query, (err, days) => {
+                if (err) return res.send(err);
+                res.status(200).send(days.sort(daySortCallback));
+            });
+        } else {
+            Day.find({ student: req.user.id, cohort: req.user.cohortId, ...req.query }, (err, days) => {
+                if (err) return res.send(err);
+                res.status(200).send(days.sort(daySortCallback));
+            })
+        }
     })
     .post((req, res) => {
-        const { inSession } = req.query;
-        let newDay;
-        if (inSession)
-            newDay = new InSessionDay(req.body);
-        else
-            newDay = new Day(req.body);
-        newDay.save((err, savedDay) => {
-            if (err) return res.send(err);
-            res.status(201).send(savedDay);
-        });
+        InSessionDay.insertMany(req.body.inSessions, (err, inSessions) => {
+            if (err) return res.status(500).send(err);
+            Day.insertMany(req.body.noSessions, (err, noSessions) => {
+                if (err) return res.status(500).send(err);
+                res.status(201).send([...inSessions, ...noSessions].sort(daySortCallback));
+            })
+        })
+
     });
 
 module.exports = dayRouter;
--- routes/api/utils.js
+++ routes/api/utils.js
@@ -1,3 +1,3 @@
 module.exports = {
-    
+    daySortCallback : (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
 }
