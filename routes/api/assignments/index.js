56a729dd4dd28ab5e621c696834f1ca7650b34e2
Ben Turner <bbgrabbag@gmail.com>
2018-05-06 00:50:28 -0600

    removed unused modules

--- routes/api/assignments/index.js
+++ routes/api/assignments/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const assignmentRouter = express.Router();
 
 const Assignment = require("../../../models/api/assignments/");
-const { Question } = require("../../../models/api/course-material/questions");
-const { AssignmentQ } = require("../../../models/api/assignments/questions");
 
 assignmentRouter.route("/")
     .get((req, res) => {
-        if (req.user.permissions.admin) {
+        if (req.user.admin) {
             AssignmentsModel.find(req.query, (err, assignments) => {
                 if (err) return res.status(500).send(err);
                 res.status(200).send(assignments);
@@ -19,7 +17,6 @@ assignmentRouter.route("/")
             })
         }
     })
-    // POST multiple assignments for each student, per cohort
     .post((req, res) => {
         Assignment.insertMany(req.body, (err, assignments) => {
             if (err) return res.status(500).send(err);
